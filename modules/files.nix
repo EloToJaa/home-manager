694e40556a7375d1d22d6eefa51b03e57b82eec1
Robert Helgesson <robert@rycee.net>
2026-05-07 23:30:43 +0200

    fixup! WIP home-manager: use putter for file management

--- modules/files.nix
+++ modules/files.nix
@@ -90,6 +90,31 @@ in
       '';
     };
 
+    home.fileActivator = lib.mkOption {
+      type =
+        with lib.types;
+        enum [
+          "legacy"
+          "putter"
+        ];
+      default = "legacy";
+      example = "putter";
+      visible = false;
+      description = ''
+        The tooling to use to place files during activation.
+
+        The legacy option (currently the default) is the built-in tooling that
+        is very robust, but is limited in future potential.
+
+        The putter option is a new external tool that may replace the legacy
+        alternative in the future. It is not as hardened as the legacy
+        alternative but will allow future features such as file copying.
+
+        This option should be considered experimental and is therefore hidden
+        from documentation at this time.
+      '';
+    };
+
     home-files = lib.mkOption {
       type = lib.types.package;
       internal = true;
@@ -149,17 +174,192 @@ in
 
     # This verifies that the links we are about to create will not
     # overwrite an existing file.
-    home.activation.checkLinkTargets = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
-      ${lib.getExe pkgs.putter} check -v \
-        --state-file "${putterStatePath}" \
-        ${config.home.internal.filePutterConfig}
-    '';
+    home.activation.checkLinkTargets = lib.hm.dag.entryBefore [ "writeBoundary" ] (
+      let
+        # Paths that should be forcibly overwritten by Home Manager.
+        # Caveat emptor!
+        forcedPaths = lib.concatMapStringsSep " " (p: ''"$HOME"/${lib.escapeShellArg p}'') (
+          map (v: v.target) (lib.filter (v: v.force) cfg)
+        );
 
-    home.activation.linkGeneration = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ${lib.getExe pkgs.putter} apply $VERBOSE_ARG -v ''${DRY_RUN:+--dry-run} \
-        --state-file "${putterStatePath}" \
-        ${config.home.internal.filePutterConfig}
-    '';
+        storeDir = lib.escapeShellArg builtins.storeDir;
+
+        legacyCheckScript = pkgs.replaceVars ./files/check-link-targets.sh {
+          inherit (config.lib.bash) initHomeManagerLib;
+          inherit forcedPaths storeDir;
+        };
+
+        legacyCheckLinkTargets = ''
+          function checkNewGenCollision() {
+            local newGenFiles
+            newGenFiles="$(readlink -e "$newGenPath/home-files")"
+            find "$newGenFiles" \( -type f -or -type l \) \
+                -exec bash ${legacyCheckScript} "$newGenFiles" {} +
+          }
+
+          checkNewGenCollision || exit 1
+        '';
+
+        putterCheckLinkTargets = ''
+          ${lib.getExe pkgs.putter} check -v \
+            --state-file "${putterStatePath}" \
+            ${config.home.internal.filePutterConfig}
+        '';
+      in
+      if cfg.fileActivator == "putter" then putterCheckLinkTargets else legacyCheckLinkTargets
+    );
+
+    # This activation script will
+    #
+    # 1. Remove files from the old generation that are not in the new
+    #    generation.
+    #
+    # 2. Symlink files from the new generation into $HOME.
+    #
+    # This order is needed to ensure that we always know which links
+    # belong to which generation. Specifically, if we're moving from
+    # generation A to generation B having sets of home file links FA
+    # and FB, respectively then cleaning before linking produces state
+    # transitions similar to
+    #
+    #      FA   →   FA ∩ FB   →   (FA ∩ FB) ∪ FB = FB
+    #
+    # and a failure during the intermediate state FA ∩ FB will not
+    # result in lost links because this set of links are in both the
+    # source and target generation.
+    home.activation.linkGeneration = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+      let
+        storeDir = lib.escapeShellArg builtins.storeDir;
+
+        legacyLink = pkgs.writeShellScript "link" ''
+          ${config.lib.bash.initHomeManagerLib}
+
+          newGenFiles="$1"
+          shift
+          for sourcePath in "$@" ; do
+            relativePath="''${sourcePath#$newGenFiles/}"
+            targetPath="$HOME/$relativePath"
+            if [[ -e "$targetPath" && ! -L "$targetPath" ]] ; then
+              if [[ -n "$HOME_MANAGER_BACKUP_COMMAND" ]] ; then
+                verboseEcho "Running $HOME_MANAGER_BACKUP_COMMAND $targetPath."
+                run $HOME_MANAGER_BACKUP_COMMAND "$targetPath" || errorEcho "Running `$HOME_MANAGER_BACKUP_COMMAND` on '$targetPath' failed."
+              elif [[ -n "$HOME_MANAGER_BACKUP_EXT" ]] ; then
+                # The target exists, back it up
+                backup="$targetPath.$HOME_MANAGER_BACKUP_EXT"
+                if [[ -e "$backup" && -n "$HOME_MANAGER_BACKUP_OVERWRITE" ]]; then
+                  run rm $VERBOSE_ARG "$backup"
+                fi
+                run mv $VERBOSE_ARG "$targetPath" "$backup" || errorEcho "Moving '$targetPath' failed!"
+              fi
+            fi
+
+            if [[ -e "$targetPath" && ! -L "$targetPath" ]] && cmp -s "$sourcePath" "$targetPath" ; then
+              # The target exists but is identical – don't do anything.
+              verboseEcho "Skipping '$targetPath' as it is identical to '$sourcePath'"
+            else
+              # Place that symlink, --force
+              # This can still fail if the target is a directory, in which case we bail out.
+              run mkdir -p $VERBOSE_ARG "$(dirname "$targetPath")"
+              run ln -Tsf $VERBOSE_ARG "$sourcePath" "$targetPath" || exit 1
+            fi
+          done
+        '';
+
+        legacyCleanup = pkgs.writeShellScript "cleanup" ''
+          ${config.lib.bash.initHomeManagerLib}
+
+          # A symbolic link whose target path matches this pattern will be
+          # considered part of a Home Manager generation.
+          homeFilePattern="$(readlink -e ${storeDir})/*-home-manager-files/*"
+
+          newGenFiles="$1"
+          shift 1
+          for relativePath in "$@" ; do
+            targetPath="$HOME/$relativePath"
+            if [[ -e "$newGenFiles/$relativePath" ]] ; then
+              verboseEcho "Checking $targetPath: exists"
+            elif [[ ! "$(readlink "$targetPath")" == $homeFilePattern ]] ; then
+              warnEcho "Path '$targetPath' does not link into a Home Manager generation. Skipping delete."
+            else
+              verboseEcho "Checking $targetPath: gone (deleting)"
+              run rm $VERBOSE_ARG "$targetPath"
+
+              # Recursively delete empty parent directories.
+              targetDir="$(dirname "$relativePath")"
+              if [[ "$targetDir" != "." ]] ; then
+                pushd "$HOME" > /dev/null
+
+                # Call rmdir with a relative path excluding $HOME.
+                # Otherwise, it might try to delete $HOME and exit
+                # with a permission error.
+                run rmdir $VERBOSE_ARG \
+                    -p --ignore-fail-on-non-empty \
+                    "$targetDir"
+
+                popd > /dev/null
+              fi
+            fi
+          done
+        '';
+
+        # This activation script will
+        #
+        # 1. Remove files from the old generation that are not in the new
+        #    generation.
+        #
+        # 2. Symlink files from the new generation into $HOME.
+        #
+        # This order is needed to ensure that we always know which links
+        # belong to which generation. Specifically, if we're moving from
+        # generation A to generation B having sets of home file links FA
+        # and FB, respectively then cleaning before linking produces state
+        # transitions similar to
+        #
+        #      FA   →   FA ∩ FB   →   (FA ∩ FB) ∪ FB = FB
+        #
+        # and a failure during the intermediate state FA ∩ FB will not
+        # result in lost links because this set of links are in both the
+        # source and target generation.
+        legacyLinkGeneration = ''
+          function linkNewGen() {
+            _i "Creating home file links in %s" "$HOME"
+
+            local newGenFiles
+            newGenFiles="$(readlink -e "$newGenPath/home-files")"
+            find "$newGenFiles" \( -type f -or -type l \) \
+              -exec bash ${legacyLink} "$newGenFiles" {} +
+          }
+
+          function cleanOldGen() {
+            if [[ ! -v oldGenPath || ! -e "$oldGenPath/home-files" ]] ; then
+              return
+            fi
+
+            _i "Cleaning up orphan links from %s" "$HOME"
+
+            local newGenFiles oldGenFiles
+            newGenFiles="$(readlink -e "$newGenPath/home-files")"
+            oldGenFiles="$(readlink -e "$oldGenPath/home-files")"
+
+            # Apply the cleanup script on each leaf in the old
+            # generation. The find command below will print the
+            # relative path of the entry.
+            find "$oldGenFiles" '(' -type f -or -type l ')' -printf '%P\0' \
+              | xargs -0 bash ${legacyCleanup} "$newGenFiles"
+          }
+
+          cleanOldGen
+          linkNewGen
+        '';
+
+        putterLinkGeneration = ''
+          ${lib.getExe pkgs.putter} apply $VERBOSE_ARG -v ''${DRY_RUN:+--dry-run} \
+            --state-file "${putterStatePath}" \
+            ${config.home.internal.filePutterConfig}
+        '';
+      in
+      if cfg.fileActivator == "putter" then putterLinkGeneration else legacyLinkGeneration
+    );
 
     home.activation.checkFilesChanged = lib.hm.dag.entryBefore [ "linkGeneration" ] (
       let
